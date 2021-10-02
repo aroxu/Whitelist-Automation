c81577c8e9e78d0d46c5d1d4a7d4751af0513f21
aroxu <zeus8502@gmail.com>
2021-10-02 18:57:38 +0900

    :bug: Null 체크 추가

--- src/main/kotlin/me/aroxu/wa/discord/WADiscordMessageHandler.kt
+++ src/main/kotlin/me/aroxu/wa/discord/WADiscordMessageHandler.kt
@@ -6,7 +6,7 @@ import dev.kord.core.on
 object WADiscordMessageHandler {
     fun onMessage() {
         WADiscordClient.client.on<MessageCreateEvent> {
-            if(!WADiscordClient.admins.contains(message.author!!.id.asString)) {
+            if(!WADiscordClient.admins.contains(message.author?.id?.asString)) {
                 return@on
             }
             if(message.author!!.isBot) {
