b91e1d43e12f184a0d6e88897511db00a2e7e32e
aroxu <zeus8502@gmail.com>
2022-01-04 00:57:10 +0900

    :arrow_up: 의존성 업그레이드

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,15 +8,16 @@ plugins {
 repositories {
     maven("https://repo.maven.apache.org/maven2/")
     maven("https://papermc.io/repo/repository/maven-public/")
+    mavenCentral()
 }
 val kordVersion: String by project
 dependencies {
     compileOnly(kotlin("stdlib-jdk8:1.5.21"))
     compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
-    compileOnly("io.github.monun:kommand-api:2.6.6")
+    compileOnly("io.github.monun:kommand-api:2.8.0")
 
-    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
-    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
+    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-native-mt")
+    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
     compileOnly("dev.kord:kord-core:$kordVersion")
 }
 
